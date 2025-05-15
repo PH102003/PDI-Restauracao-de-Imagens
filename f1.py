a50cc501518c5dbbff03f248a7924972a1ebc2be
mariojorgetimoteo <111126284+mariojorgetimoteo@users.noreply.github.com>
2025-05-15 10:12:41 -0300

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,1 +1,15 @@
+import cv2
 
+
+input_image = cv2.imread('lena_ruido.png')
+
+
+kernel_size = 5  
+denoised_image = cv2.medianBlur(input_image, kernel_size)
+
+cv2.imwrite('denoised_image.jpg', denoised_image)
+
+cv2.imshow('Original Noisy Image', input_image)
+cv2.imshow('Denoised Image', denoised_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
